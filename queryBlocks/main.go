baa38f965a663a5f380d0bdb895f7f684366dabb
jeomiah <ehdaorwn3192@gmail.com>
2022-05-29 13:05:55 +0900

    querying transaction in the block

--- queryBlocks/main.go
+++ queryBlocks/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -42,5 +43,29 @@ func main() {
 	}
 
 	fmt.Println(count)
+
+	blockHash := common.HexToHash("0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9")
+	count, err = client.TransactionCount(context.Background(), blockHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for idx := uint(0); idx < count; idx++ {
+		tx, err := client.TransactionInBlock(context.Background(), blockHash, idx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(tx.Hash().Hex())
+	}
+
+	txHash := common.HexToHash("0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2")
+	tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(tx.Hash().Hex())
+	fmt.Println(isPending)
 
 }
